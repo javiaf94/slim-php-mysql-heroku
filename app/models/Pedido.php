8f64bbc804b50fb0523a2f4c00d98dfc90de1de9
javiaf94 <javi_af94@hotmail.com>
2021-11-11 20:02:21 -0300

    agrego actualizar pedido

--- app/models/Pedido.php
+++ app/models/Pedido.php
@@ -46,13 +46,13 @@ class Pedido
     public static function obtenerPorComandaMesa($com_codigo, $mesa_codigo)
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-    $consulta = $objAccesoDatos->prepararConsulta("SELECT prd_nombre as nombre_producto, 
+        $consulta = $objAccesoDatos->prepararConsulta("SELECT prd_nombre as nombre_producto, 
                                                           cantidad, ped.estado, tiempo_preparacion
-                                                   FROM pedido_ped ped
-                                                   INNER JOIN comanda_com com
-                                                   on com.codigo = ped.com_codigo
-                                                   WHERE ped.com_codigo = :com_codigo
-                                                   AND com.mes_codigo = :mes_codigo");
+                                                        FROM pedido_ped ped
+                                                        INNER JOIN comanda_com com
+                                                        on com.codigo = ped.com_codigo
+                                                        WHERE ped.com_codigo = :com_codigo
+                                                        AND com.mes_codigo = :mes_codigo");
         $consulta->bindValue(':com_codigo', $com_codigo, PDO::PARAM_STR);
         $consulta->bindValue(':mes_codigo', $mesa_codigo, PDO::PARAM_INT);
 
@@ -61,6 +61,21 @@ class Pedido
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
     
+    public static function actualizarEstadoTiempo($com_codigo, $prd_nombre, $estado, $tiempo_preparacion)
+    {
+        $objAccesoDatos = AccesoDatos::obtenerInstancia();
+        $consulta = $objAccesoDatos->prepararConsulta("UPDATE pedido_ped SET estado=:estado,    
+                                                      tiempo_preparacion=:tiempo_preparacion,
+                                                      WHERE com_codigo=:com_codigo AND prd_nombre=:prd_nombre");
+        $consulta->bindValue(':com_codigo', $com_codigo, PDO::PARAM_STR);
+        $consulta->bindValue(':prd_nombre', $prd_nombre, PDO::PARAM_STR);
+        $consulta->bindValue(':estado', $estado, PDO::PARAM_STR);
+        $consulta->bindValue(':tiempo_preparacion', $tiempo_preparacion, PDO::PARAM_INT);
+
+        $consulta->execute();
+        return $consulta->rowCount();
+    }
+
 }
 
 
